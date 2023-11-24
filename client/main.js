467771ca112483a2378b5669ff0d91c379d38d21
Lulurem <smile71109@naver.com>
2023-11-24 09:23:29 +0900

    [add] Basic 학습
    - codeStructure 학습

--- client/main.js
+++ client/main.js
@@ -1,3 +1,6 @@
-const a = "hello";
+console.log("13");
 
-console.log(navigator.language);
+// alert('마우스 우클릭은 사용할 수 없습니다.')
+
+// console.log( prompt('당신의 이름은?')  );
+console.log(window.confirm("폭파하시겠습니까?"));
