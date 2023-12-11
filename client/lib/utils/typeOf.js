9e2192b5673a0381d1c107699f0d9bc5f0d412c0
Lulurem <smile71109@naver.com>
2023-12-11 14:38:12 +0900

    js core

---
+++ client/lib/utils/typeOf.js
@@ -0,0 +1,17 @@
+function isObject(data) {
+  return (
+    Object.prototype.toString.call(data).slice(8, -1).toLowerCase() === "object"
+  );
+}
+
+function cloneDeep(object) {
+  return Object.fromEntries(
+    Object.entries(object).map(([key, value]) => {
+      let type = typeof value;
+      if (value && type === "object") {
+        value = cloneDeep(value);
+      }
+      return [key, value];
+    })
+  );
+}
